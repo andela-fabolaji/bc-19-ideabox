d85982b15dd211e1fd9b900f78b6d09e01e3e016
femipixels <femi.systems@gmail.com>
2016-08-25 03:55:41 +0100

    fixed bugs on index view

--- app.js
+++ app.js
@@ -23,18 +23,3 @@ app.use("*", function (req, res) {
 app.listen(3000, function () {
   console.log("Server running at Port 3000");
 });
-
-// var express = require('express');
-// var app = express();
-// var port = 3000
-
-// app.use(express.static('public'));
-// app.use(express.static('src/views'));
-
-// app.get('/', function (req, res) {
-//   //res.send(views + '404.html');
-// });
-
-// app.listen(port, function (err) {
-//   console.log('Server running at ', port);
-// });
