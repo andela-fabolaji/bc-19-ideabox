c91dfec637b3ba302e8775dff70d18d2a733f58f
femipixels <femi.systems@gmail.com>
2016-08-28 03:00:59 +0100

    Implemented the upvotes and downvotes

--- route/controllers/dbcontroller.js
+++ route/controllers/dbcontroller.js
@@ -114,7 +114,7 @@ var DbHandler = function () {
 
   this.getIdeas = function (response) {
     var sorter = 'date';
-    var query = 'SELECT *, ideas.id AS ideaId FROM ideas, users WHERE users.id = users_id ORDER BY ' + this.connection.escapeId('ideas.' + sorter) + ' DESC';
+    var query = 'SELECT *, ideas.id AS ideaId, SUM(votes.upvotes) AS upvotes, SUM(votes.downvotes) AS downvotes FROM ideas INNER JOIN users on users.id = users_id LEFT JOIN votes ON ideas.id = ideas_id GROUP BY ideas.id ORDER BY ' + this.connection.escapeId('ideas.' + sorter) + ' DESC';
     this.connection.getConnection(function (err, connection) {
       connection.query(query, function (err, result) {
         if (!err) {
@@ -139,6 +139,69 @@ var DbHandler = function () {
     var query = 'SELECT * FROM comments '
   };
 
+  this.votes = function(data, response) {
+    var query = 'SELECT * FROM votes WHERE users_id = '+data.users_id +' AND ideas_id = '+data.ideas_id;
+    if(data.type === 'upvotes') {
+      data.upvotes = 1;
+    } else {
+      data.downvotes = 1;
+    }
+    var type = data.type;
+    delete data['type'];
+    var thisInstance = this
+    thisInstance.connection.getConnection(function(err, connection) {
+      connection.query(query,function(err, result) {
+        if(result.length === 0) {
+          insert = thisInstance.insertQuery('votes', data, response);
+          connection.query(insert, function(err, result) {
+            if(!err) {
+              response.json({'value':1, 'type':type});
+            }
+          })
+        } else {
+          if((result[0].upvotes == 1 && type === 'upvotes') || (result[0].downvotes == 1 && type === 'downvotes')) {
+            var deleteQuery = 'DELETE FROM votes WHERE id = '+result[0].id;
+            connection.query(deleteQuery, function(err, result) {
+              if(!err) {
+                response.json({'value':-1, 'type':type});
+              }
+            })
+          } else {
+            if(type === 'upvotes') data.downvotes = 0;
+            else data.upvotes = 0;
+            var update = thisInstance.updateTable('votes', data, {id:result[0].id});
+            connection.query(update, function(err, result){
+              if(!err) {
+                response.json({'value':1, 'type':'both'});
+              }
+            })
+          }
+        }
+      })
+    })
+  }
+
+  this.updateTable = function(tableName, data, whereClause) {
+    var query = "UPDATE "+tableName+" SET ";
+    var startFlag = true;
+    for(var key in data) {
+      if(!startFlag) {
+        query += ', ';
+      }
+      query += key +" = '"+data[key]+"'";
+      startFlag = false;
+    }
+    query += ' WHERE ';
+    startFlag = true;
+    for(var key in whereClause) {
+      if(!startFlag) {
+        query += ' AND ';
+      }
+      query += key +" = '"+whereClause[key]+"'";
+      startFlag = false;
+    }
+    return query;
+  }
 };
 
 module.exports = DbHandler;
